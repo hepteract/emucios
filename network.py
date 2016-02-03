0235760afb16ff4f1484b51dff8b2c212ea8dae6
hepteract <elijah@shivergaming.com>
2016-02-02 21:04:46 -0800

    Update package management system

--- network.py
+++ network.py
@@ -2,13 +2,26 @@
 
 import requests
 
+class Response(object):
+    def __init__(self, response):
+        self.__response = response
+
+    def __repr__(self):
+        return self.__response.text
+
+    def __getattr__(self, name):
+        return getattr(self.__response, name)
+
+    def __getitem__(self, name):
+        return self.__response[name]
+
 class CurlInterface(object):
     def __init__(self):
         self.ret = None
     def __getitem__(self, name):
         try:
-            return requests.get(\
-                "http://" + name.replace(";", "/").replace("\;", ";"))
+            return Response(requests.get(\
+                "http://" + name.replace(";", "/").replace("\;", ";")))
         except:
             return -1
 
